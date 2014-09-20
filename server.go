5eb5b458d2f4b7920bdbade88007ae23dd040870
claudiu <claudiu.garba@gmail.com>
2014-09-20 17:24:08 +0200

    server

--- server.go
+++ server.go
@@ -2,71 +2,51 @@ package main
 
 import (
 	"database/sql"
+	_ "database/sql"
 	"fmt"
+	_ "fmt"
+	"github.com/gin-gonic/gin"
 	_ "github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	_ "log"
 )
 
 func main() {
 
-	var (
-		id    int
-		title string
-		description string
-	)
+	type Lesson struct {
+		Id          int    `json:"id"`
+		Title       string `json:"title"`
+		Description string `json:"description"`
+	}
 
 	// DB STUFF //
-	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/test")
-	if err != nil {
-		log.Fatal(err)
-	}
+	//
+	router := gin.Default()
 
-	rows, err := db.Query("select * from lessons")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for rows.Next() {
-		err := rows.Scan(&id, &title, &description)
+	router.GET("/", func(c *gin.Context) {
+		db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/test")
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(id, title, description)
-	}
 
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+		rows, err := db.Query("select * from lessons")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-//	insert, err := db.Prepare("insert into lessons(description, title) values(?, ?)")
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	res, err := insert.Exec("Dolly", "someother stuff")
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	fmt.Println(res.RowsAffected())
-
-	defer db.Close()
-	//	router := gin.Default()
-
-	//	router.GET("/")
-	//	//	LESSONS //
-	//	router.GET("/lessons")
-	//	router.GET("/lessons/:id")
-	//	// USERS //
-	//	router.GET("/users")
-	//	router.GET("/users/:id")
-	//	// LEVELS //
-	//	router.GET("/levels")
-	//	router.GET("/levels/:id")
-	//	// SCORES //
-	//	router.GET("/scores")
-	//	router.GET("/scores/:id")
-	//
-	//	router.Run(":8000")
+		results := []Lesson{}
+		for rows.Next() {
+			result := Lesson{}
+			err := rows.Scan(&result.Id, &result.Title, &result.Description)
+			results = append(results, result)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		c.JSON(200, results)
+		defer db.Close()
+		fmt.Println(results)
+	})
+	router.Run(":3001")
 }
